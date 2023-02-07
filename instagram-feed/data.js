90850fc19285558b2cb4d90f74be335acb271f04
Jon Doe <jondoe@doe.com>
2023-02-08 01:36:52 +0800

    step2-add initial feed data

---
+++ instagram-feed/data.js
@@ -0,0 +1,13 @@
+const initialData = {
+    articles: [
+      {
+        id: 1,
+        avatar: '',
+        name: 'John Doe',
+        image: '',
+        likes: 100,
+        commentCount: 10,
+        comments:""
+      }
+    ]
+}

9407d23e36a9da82694af8b6822a9bfb33deb2b7
Jon Doe <jondoe@doe.com>
2023-02-08 01:51:21 +0800

    step3-import data into app.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import initialData from './instagram-feed/data'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {console.log(initialData.articles[0].name)}
       <Text>Instagram Feed App</Text>
       <StatusBar style="auto" />
     </View>
--- instagram-feed/data.js
+++ instagram-feed/data.js
@@ -8,6 +8,44 @@ const initialData = {
         likes: 100,
         commentCount: 10,
         comments:""
-      }
-    ]
+      },
+      {
+        id: 2,
+        avatar: '',
+        name: 'Johnny Doe',
+        image: '',
+        likes: 100,
+        commentCount: 10,
+        comments:""
+      },
+      {
+        id: 3,
+        avatar: '',
+        name: 'John Doee',
+        image: '',
+        likes: 100,
+        commentCount: 10,
+        comments:""
+      },
+      {
+        id: 4,
+        avatar: '',
+        name: 'June Doe',
+        image: '',
+        likes: 100,
+        commentCount: 10,
+        comments:""
+      },
+      {
+        id: 5,
+        avatar: '',
+        name: 'John papa',
+        image: '',
+        likes: 100,
+        commentCount: 10,
+        comments:""
+      },
+    ],
 }
+
+export default initialData;

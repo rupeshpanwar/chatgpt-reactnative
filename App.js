a499cfc64cb499bd1fb3e8d7a30ed2e0d0c5c7a8
Jon Doe <jondoe@doe.com>
2023-02-08 23:21:12 +0800

    step5-adding the image and rendering the list

--- App.js
+++ App.js
@@ -1,44 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , FlatList} from 'react-native';
-import initialData from './instagram-feed/data'
+import { FlatList, StyleSheet, Text, View , Image } from 'react-native';
+import data from './instagram-feed/data';
 
 
-const renderItem = ({ item }) => (
+export default function Instagram() {
+return (
   <View>
+  <View style={styles.container}>
+  <Text>Instagram App {'\n'}</Text>
+  <StatusBar style="auto" />
+  </View>
+<FlatList
+data={data.articles}
+renderItem={({item}) => (
+ <View style={styles.itemContainer}>
+  <Image 
+    style={styles.image}
+    source={item.image}
+  />
   <Text>name: {item.name}</Text>
   <Text>likes: {item.likes}</Text>
-  <Text>comments: {item.comment}</Text>
+  <Text>comments: {item.comments}</Text>
   </View>
-  );
+)}
 
-
-export default function App() {
-return (
-<FlatList
-data={initialData.articles}
-renderItem={renderItem}
-keyExtractor={(item) => item.name}
+keyExtractor = {(item) => item.id.toString()}
+showsVerticalScrollIndicator={false}
 />
+<StatusBar style="auto" />
+</View>
 );
-}
-// export default function App() {
+_}
+
+
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+backgroundColor: '#fff',
+alignItems: 'center',
+justifyContent: 'center',
+},
+image: {
+width: '100%',
+height: 200,
+resizeMode: 'contain',
+marginVertical: 10,
+},
+});
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
+// import data from './instagram-feed/data';
+
+// export default function Instagram() {
 //   return (
-//     <View style={styles.container}>
-//       <Text>Instagram Feed App</Text>
-//       <Text>initialData.articles[0].name</Text>
-//       <Text>initialData.articles[0].likes</Text>
-//       <Text>initialData.articles[0].comment</Text>
+//     <View>
+//       <View style={styles.container}>
+//         <Text>Instagram App</Text>
+//         <StatusBar style="auto" />
+//       </View>
+//       <FlatList
+//         data={data.articles}
+//         renderItem={({ item }) => (
+//           <View style={styles.itemContainer}>
+//             <Image
+//               style={styles.image}
+//               source={{ uri: item.image }}
+//             />
+//             <Text>name: {item.name}</Text>
+//             <Text>likes: {item.likes}</Text>
+//             <Text>comments: {item.commentCount}</Text>
+//           </View>
+//         )}
+//         keyExtractor={(item) => item.id.toString()}
+//         showsVerticalScrollIndicator={false}
+//       />
 //       <StatusBar style="auto" />
 //     </View>
 //   );
 // }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   image: {
+//     width: '100%',
+//     height: 200,
+//     resizeMode: 'contain',
+//     marginVertical: 10,
+//   },
+// });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- instagram-feed/data.js
+++ instagram-feed/data.js
@@ -1,8 +1,37 @@
 const initialData = {
+
+    profile: {
+      avatar: require("../assets/instagram-feed-images/avatar.jpeg")
+    },
+
+    stories: [
+      {
+        id: 1,
+        avatar: require("../assets/instagram-feed-images/avatar.jpeg"),
+        name: 'John Doe',
+        isSeen: false,
+
+      },
+      {
+        id: 2,
+        avatar: require("../assets/instagram-feed-images/avatar2.jpg"),
+        name: 'Johnny Doe',
+        isSeen: false,
+
+      },
+      {
+        id: 3,
+        avatar: require("../assets/instagram-feed-images/avatar3.jpg"),
+        name: 'John Doee',
+        isSeen: true,
+
+      },
+
+    ],
     articles: [
       {
         id: 1,
-        avatar: 'require("../assets/instagram-feed-images/avatar.jpeg")',
+        avatar: require("../assets/instagram-feed-images/avatar.jpeg"),
         name: 'John Doe',
         image: require("../assets/instagram-feed-images/img1.jpg"),
         likes: 100,
@@ -13,25 +42,25 @@ const initialData = {
         id: 2,
         avatar: 'require("../assets/instagram-feed-images/avatar1.jpg")',
         name: 'Johnny Doe',
-        image: 'require("../assets/instagram-feed-images/img2.jpg")',
+        image: require("../assets/instagram-feed-images/img2.jpg"),
         likes: 1,
         commentCount: 30,
         comments:""
       },
       {
         id: 3,
-        avatar: 'require("../assets/instagram-feed-images/avatar3.jpeg")',
+        avatar: require("../assets/instagram-feed-images/avatar3.jpg"),
         name: 'John Doee',
-        image: 'require("../assets/instagram-feed-images/img4.jpg")',
+        image: require("../assets/instagram-feed-images/img4.jpg"),
         likes: 1000,
         commentCount: 1,
         comments:""
       },
       {
         id: 4,
-        avatar: 'require("../assets/instagram-feed-images/avatar4.jpeg")',
+        avatar: require("../assets/instagram-feed-images/img4.jpg"),
         name: 'June Doe',
-        image: 'require("../assets/instagram-feed-images/img4.jpg")',
+        image: require("../assets/instagram-feed-images/img4.jpg"),
         likes: 500,
         commentCount: 21,
         comments:""
@@ -39,4 +68,13 @@ const initialData = {
     ],
 }
 
-export default initialData;
+const data = {
+  ...initialData,
+  articles: initialData.articles.map( article => {
+    return {
+      ...article,
+      comment: 'View all $${article.commintCount} comments'
+    }
+  })
+}
+export default data;

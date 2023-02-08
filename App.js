9d1516bf1cbb3deb3f50faa619d39523c1f44f0e
Jon Doe <jondoe@doe.com>
2023-02-08 19:46:07 +0800

    step4-create flatlist to display all the items

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , FlatList} from 'react-native';
 import initialData from './instagram-feed/data'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      {console.log(initialData.articles[0].name)}
-      <Text>Instagram Feed App</Text>
-      <StatusBar style="auto" />
-    </View>
+
+const renderItem = ({ item }) => (
+  <View>
+  <Text>name: {item.name}</Text>
+  <Text>likes: {item.likes}</Text>
+  <Text>comments: {item.comment}</Text>
+  </View>
   );
+
+
+export default function App() {
+return (
+<FlatList
+data={initialData.articles}
+renderItem={renderItem}
+keyExtractor={(item) => item.name}
+/>
+);
 }
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Instagram Feed App</Text>
+//       <Text>initialData.articles[0].name</Text>
+//       <Text>initialData.articles[0].likes</Text>
+//       <Text>initialData.articles[0].comment</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
 
 const styles = StyleSheet.create({
   container: {
--- instagram-feed/data.js
+++ instagram-feed/data.js
@@ -2,49 +2,40 @@ const initialData = {
     articles: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'require("../assets/instagram-feed-images/avatar.jpeg")',
         name: 'John Doe',
-        image: '',
+        image: require("../assets/instagram-feed-images/img1.jpg"),
         likes: 100,
         commentCount: 10,
         comments:""
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'require("../assets/instagram-feed-images/avatar1.jpg")',
         name: 'Johnny Doe',
-        image: '',
-        likes: 100,
-        commentCount: 10,
+        image: 'require("../assets/instagram-feed-images/img2.jpg")',
+        likes: 1,
+        commentCount: 30,
         comments:""
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'require("../assets/instagram-feed-images/avatar3.jpeg")',
         name: 'John Doee',
-        image: '',
-        likes: 100,
-        commentCount: 10,
+        image: 'require("../assets/instagram-feed-images/img4.jpg")',
+        likes: 1000,
+        commentCount: 1,
         comments:""
       },
       {
         id: 4,
-        avatar: '',
+        avatar: 'require("../assets/instagram-feed-images/avatar4.jpeg")',
         name: 'June Doe',
-        image: '',
-        likes: 100,
-        commentCount: 10,
+        image: 'require("../assets/instagram-feed-images/img4.jpg")',
+        likes: 500,
+        commentCount: 21,
         comments:""
-      },
-      {
-        id: 5,
-        avatar: '',
-        name: 'John papa',
-        image: '',
-        likes: 100,
-        commentCount: 10,
-        comments:""
-      },
+      }
     ],
 }
 
